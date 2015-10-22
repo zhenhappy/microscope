82d9087a4f04223d4295aa72474a0b91c5db67f1
zhenhappy <q505507538@gmail.com>
2015-10-22 12:34:24 +0800

    添加帖子编辑表单

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,4 +1,8 @@
 Template.postItem.helpers({
+  // 确认当前用户是帖子提供者
+  ownPost: function() {
+    return this.userId === Meteor.userId();
+  },
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
