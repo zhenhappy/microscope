8c91b0fdd194fe402d4d2a5bb148a07034bb3ba1
zhenhappy <q505507538@gmail.com>
2015-10-22 19:46:58 +0800

    添加插入修改的安全性验证

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -9,13 +9,20 @@ Template.postEdit.events({
       title: $(e.target).find('[name=title]').val()
     }
 
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
-      if (error) {
-        // 向用户显示错误信息
-        alert(error.reason);
-      } else {
-        Router.go('postsList');
-      }
+    Meteor.call('postUpdate', currentPostId, postProperties, function(error, result) {
+      // 显示错误信息并退出
+      if (error)
+        return alert(error.reason);
+
+      // 用户非法越界操作
+      if (result.userInvalid)
+        return alert('User\'s illegal operation（用户非法越界操作）');
+
+      // 显示结果，跳转页面
+      if (result.postExists)
+        return alert('This link has already been posted（该链接已经存在）');
+
+      Router.go('postsList');
     });
   },
 
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -12,6 +12,10 @@ Template.postSubmit.events({
       if (error)
         return alert(error.reason);
 
+      // 用户非法越界操作
+      if (result.userInvalid)
+        return alert('User\'s illegal operation（用户非法越界操作）');
+
       // 显示结果，跳转页面
       if (result.postExists)
         alert('This link has already been posted（该链接已经存在）');
